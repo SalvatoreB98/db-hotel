4e263c7d65049149ca790f2670f71f5fdd512701
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-06-11 10:38:15 +0200

    Correzione es. 4

--- queryJoin.sql
+++ queryJoin.sql
@@ -1,24 +1,25 @@
 -- 1.
-SELECT ospite_id,COUNT(ospite_id),ospiti.name,ospiti.lastname 
+SELECT ospite_id,COUNT(ospite_id) AS `NUM_PRENOTAZIONI`,ospiti.name,ospiti.lastname 
 FROM `prenotazioni_has_ospiti` 
-INNER JOIN `ospiti` 
+LEFT JOIN `ospiti` 
 ON prenotazioni_has_ospiti.ospite_id = ospiti.id 
 GROUP BY ospite_id 
-HAVING COUNT(ospite_id)>=2
+HAVING NUM_PRENOTAZIONI>=2
 
 -- 2.
 SELECT `prenotazioni_has_ospiti`.id AS "prenotazione",ospite_id,`ospiti`.*
 FROM `prenotazioni_has_ospiti`
-RIGHT JOIN `ospiti` 
+LEFT JOIN `ospiti` 
 ON `prenotazioni_has_ospiti`.`ospite_id`=`ospiti`.`id`
+ORDER BY prenotazione
 
 --3.
 SELECT `pagamenti`.id,`paganti`.`name`,`paganti`.`lastname`,`pagamenti`.`price`,`prenotazioni`.`created_at`
 FROM `pagamenti`
-JOIN `prenotazioni`
+LEFT JOIN `prenotazioni`
 ON `prenotazioni`.`id` = `pagamenti`.`prenotazione_id`
-JOIN `paganti`
+LEFT JOIN `paganti`
 ON `paganti`.`id`=`pagamenti`.`pagante_id`
-WHERE `prenotazioni`.`created_at`>'2018/05/1'\
+WHERE `prenotazioni`.`created_at` BETWEEN '2018/05/01' AND '2018/05/31'
 
 --4.
