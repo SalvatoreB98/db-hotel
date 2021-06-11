09b660db8cdcbd33e13184cec898e21e2b5a9fa2
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-06-11 10:03:55 +0200

    Group by OK

--- queryGroupBy.sql
+++ queryGroupBy.sql
@@ -6,7 +6,7 @@ GROUP BY year
 --2
 SELECT SUM(`pagamenti`.`price`),`pagamenti`.`status`
 FROM `pagamenti`
-GROUP BY status
+GROUP BY `status`
 
 --3
 SELECT stanza_id AS "NUMERO STANZA",COUNT(stanza_id) AS "PRENOTATA(N VOLTE)"
@@ -14,9 +14,12 @@ FROM `prenotazioni`
 GROUP BY stanza_id
 
 --4
-
+SELECT HOUR(`created_at`) AS `ORA`, COUNT(`id`) as `prenotazioni`
+FROM `prenotazioni` 
+GROUP BY ORA
 --5
-SELECT COUNT(ospite_id) AS pren_count
+SELECT COUNT(ospite_id) AS `prenotazioni`,`ospite_id`
 FROM `prenotazioni_has_ospiti`
-GROUP BY ospite_id
-HAVING MAX(pren_count)
+GROUP BY `ospite_id`
+ORDER BY `prenotazioni` DESC
+LIMIT 1
